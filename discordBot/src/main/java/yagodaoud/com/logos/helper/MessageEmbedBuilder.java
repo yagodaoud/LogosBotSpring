f7d5595bc7620ff2ecd73e5994a792b922a54d8a
Yago Daoud <yagodaoud@hotmail.com>
2024-03-29 09:53:07 -0300

    :sparkles feature/Create MessageEmbedBuilder and Colors class

---
+++ discordBot/src/main/java/yagodaoud/com/logos/helper/MessageEmbedBuilder.java
@@ -0,0 +1,18 @@
+package yagodaoud.com.logos.helper;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+
+public abstract class MessageEmbedBuilder {
+
+    public static MessageEmbed messageEmbedBuilder(String message, Color color) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setDescription(message);
+        embedBuilder.setColor(color);
+
+        return embedBuilder.build();
+    }
+
+}
