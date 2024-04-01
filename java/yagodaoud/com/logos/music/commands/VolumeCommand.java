5b9ed5f0a92e3acaccc3b4808fea46a49c6d9b15
Yago Daoud <yagodaoud@hotmail.com>
2024-04-01 16:43:45 -0300

    :bug: fix/Create permission validation in VolumeCommand

--- java/yagodaoud/com/logos/music/commands/VolumeCommand.java
+++ java/yagodaoud/com/logos/music/commands/VolumeCommand.java
@@ -1,5 +1,6 @@
 package yagodaoud.com.logos.music.commands;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
@@ -11,7 +12,8 @@ import yagodaoud.com.logos.music.audio.PlayerManager;
 
 import java.util.List;
 
-import static yagodaoud.com.logos.music.commands.helper.PlayerNotStartedEmbedMessageBuilder.getPlayerNotStartedEmbedMessage;
+import static yagodaoud.com.logos.music.commands.helper.EmbedErrorMessageBuilder.getNotAdminEmbedMessage;
+import static yagodaoud.com.logos.music.commands.helper.EmbedErrorMessageBuilder.getPlayerNotStartedEmbedMessage;
 
 @Component
 public class VolumeCommand implements CommandHandlerInterface {
@@ -23,6 +25,10 @@ public class VolumeCommand implements CommandHandlerInterface {
 
     @Override
     public void handleCommand(SlashCommandInteractionEvent event) {
+        if (!event.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
+            event.replyEmbeds(getNotAdminEmbedMessage()).queue();
+            return;
+        }
         if (PlayerManager.getInstance() == null) {
             event.replyEmbeds(getPlayerNotStartedEmbedMessage()).queue();
             return;
