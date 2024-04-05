c4a7b18f0257e822e8887ff062d426c84b003612
Yago Daoud <yagodaoud@hotmail.com>
2024-04-05 17:28:57 -0300

    :truck: move/Add src/main directory to file - maven clean package

--- src/main/java/yagodaoud/com/logos/DiscordBot.java
+++ src/main/java/yagodaoud/com/logos/DiscordBot.java
@@ -4,16 +4,15 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import yagodaoud.com.logos.botBuilder.DiscordBotInitializer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class DiscordBot {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DiscordBot.class, args);
-		String token = Dotenv.configure().load().get("TOKENDISCORDTEST");
+		String token = Dotenv.configure().load().get("TOKENDISCORD");
 
 		JDA discordBot = DiscordBotInitializer.initBot(token, context);
 		DiscordBotInitializer.setActivity(discordBot);
