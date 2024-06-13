9da845942d8f21102279d4d64878127f00d2ecaf
yagodaoud <yagodaoud@hotmail.com>
2024-06-13 19:27:22 -0300

    :sparkles: feature/Fix webSocket sessions

--- src/main/java/yagodaoud/com/logos/webSocket/VoiceChannelWebSocketHandler.java
+++ src/main/java/yagodaoud/com/logos/webSocket/VoiceChannelWebSocketHandler.java
@@ -3,6 +3,7 @@ package yagodaoud.com.logos.webSocket;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.cdi.Eager;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -19,6 +20,23 @@ public class VoiceChannelWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(@NotNull WebSocketSession session) {
         sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(@NotNull WebSocketSession session, @NotNull CloseStatus status) {
+        sessions.remove(session);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, @NotNull Throwable exception) {
+        if (session.isOpen()) {
+            try {
+                session.close(CloseStatus.SERVER_ERROR);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        sessions.remove(session);
     }
 
     public void sendMessageToAll(String message) {
